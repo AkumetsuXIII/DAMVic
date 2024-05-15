1877cac5127e0db4fcfc41cd04516c7d3f9a2841
Victor <Victor@DESKTOP-1C1CFJE>
2024-05-15 21:32:14 +0200

    Cambio para ejercicio

--- java1/src/paquete1/Principal.java
+++ java1/src/paquete1/Principal.java
@@ -2,7 +2,7 @@ package paquete1;
 
 public class Principal {
 	public static void main(String[] args) {
-        System.out.println("Estamos haciendo control de versiones con Git");
+        System.out.println("Cambio de mensaje para el ejercicio");
     }
 
 }
